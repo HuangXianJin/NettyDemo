d428394cd64a3c131e3b7db9ddad957dd18fb5e9
huangxj <914103719@qq.com>
2017-06-28 16:55:35 +0800

    netty helloworld

---
+++ chapter1/src/main/java/server/HelloServer.java
@@ -0,0 +1,40 @@
+package server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Created by huangxj on 2017/6/28.
+ */
+public class HelloServer {
+    private static int portNumber=7878;
+
+    public static void main(String[] args){
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup);
+            b.channel(NioServerSocketChannel.class);
+            b.childHandler(new HelloServerInitializer());
+
+            // 服务器绑定端口监听
+            ChannelFuture f = b.bind(portNumber).sync();
+            // 监听服务器关闭监听
+            f.channel().closeFuture().sync();
+
+            // 可以简写为
+            /* b.bind(portNumber).sync().channel().closeFuture().sync(); */
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+
+}
